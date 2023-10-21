368472cad795118ef6385c2773f1a2077d8f0dcd
imanBekkaye <imanbekkaye@hotmail.com>
2023-10-21 14:59:32 +0200

    axios added

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,25 @@
+import axios, { AxiosRequestConfig } from 'axios';
+
+
+// ----------------------------------------------------------------------
+
+const axiosInstance = axios.create({ baseURL: 'https://api.example.com' }); // Replace with your API URL
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+);
+
+export default axiosInstance;
+
+// ----------------------------------------------------------------------
+
+export const fetcher = async (args) => {
+  const [url, config] = Array.isArray(args) ? args : [args];
+
+  const res = await axiosInstance.get(url, { ...config });
+
+  return res.data;
+};
+
+// ----------------------------------------------------------------------
