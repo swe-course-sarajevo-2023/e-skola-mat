cc1e406935b63e931ab939cf4050233fb666cdd7
Adelisa Softic <softicadelisa842@gmail.com>
2023-10-30 18:48:57 +0100

    greske pri imenovanju

--- app/api/endpoints/homeworks.py
+++ app/api/endpoints/homeworks.py
@@ -1,30 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.api import deps
-from app.database import get_db
 from app.models import HomeworkUser, ProblemUserHomework, User, Homework
 from app.schemas.requests import TaskComment, GeneralComment
 from typing import List
 
 router = APIRouter()
 
-async def get_db() -> AsyncSession:
-
-    async_engine = create_async_engine(DATABASE_URL, echo=True)
-    AsyncSessionFactory = sessionmaker(
-        bind=async_engine, class_=AsyncSession, expire_on_commit=False
-    )
-    db = AsyncSession(engine)
-    try:
-        yield db
-    finally:
-        await db.close()
-
 @router.post("/submit-homework/{homework_id}")
 def submit_homework(
     homework_id: str,
     task_comments: List[TaskComment],
     general_comment: GeneralComment,
-    db: Session = Depends(get_db),
+    db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_user),
 ):
     # Trazimo bazu
@@ -61,7 +48,7 @@ def submit_homework(
 def submit_comment(
     task_id: str,
     comment: str,
-    db: Session = Depends(get_db),
+    db: Session = Depends(deps.get_db),
     current_user: User = Depends(deps.get_current_user),  
 ):
     # Provjeriti ako task postoji
