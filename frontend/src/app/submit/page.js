c46bba1676706cd8fc58761a09764dd1e289ba68
nihadm97 <nihadm97@outlook.com>
2023-12-16 18:26:16 +0100

    deploy frontend fix 2

--- frontend/src/app/submit/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import SubmitModal from "@/views/submit-homework";
-
-export const setSubmitModalShow = (props) => {
-  console.log("PROPS", props);
-  return <SubmitModal isOpen={props} />;
-};
