b3645b811a4640c81f8223931246b055b7c816f0
nihadm97 <nihadm97@outlook.com>
2023-12-16 21:58:27 +0100

    deploy frontend fix 2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
-  async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://sample-zeaqndbcnq-oa.a.run.app/:path*',
-        },
-      ]
-    },
-};
-
 const nextConfig = {
   experimental: {
     forceSwcTransforms: true,
