26853ecf43de7ae6f2c7b7d2b9aa245fabd8ac9d
nihadm97 <nihadm97@outlook.com>
2023-12-27 23:44:46 +0100

    debug deploy test 2

--- frontend/src/app/profiles/profesor/group-student/[id]/page.js
+++ frontend/src/app/profiles/profesor/group-student/[id]/page.js
@@ -1,9 +1,12 @@
-'use client';
-import { useProtectedRoute } from '@/hooks';
-import StudentGroupView from '@/views/group-student-view';
+"use client";
+import { useProtectedRoute } from "@/hooks";
+import StudentGroupView from "@/views/group-student-view";
 
 const Ucenik = ({ params }) => {
-	return <StudentGroupView grupa={params.id} />;
+  return <StudentGroupView grupa={params.id} />;
 };
 
-export default useProtectedRoute(Ucenik);
+// Wrap the component with useProtectedRoute
+const ProtectedUcenik = useProtectedRoute(Ucenik);
+
+export default ProtectedUcenik;
