863f25cf56133caab32429fa84ad4494ffb997d8
Adelisa Softic <softicadelisa842@gmail.com>
2024-01-08 22:39:26 +0100

    fix: korigovanje importa

--- backend/app/tests/test_groups.py
+++ backend/app/tests/test_groups.py
@@ -1,5 +1,6 @@
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.models import Class
 from app.schemas.requests import ClassCreateRequest
 from main import app
