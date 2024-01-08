320476a9c4c395208d827615da0843c769e606b5
Adelisa Softic <softicadelisa842@gmail.com>
2024-01-08 22:59:32 +0100

    fix: updateovanje get group testa

--- backend/app/tests/test_groups.py
+++ backend/app/tests/test_groups.py
@@ -11,15 +11,7 @@ async def test_get_all_groups(client: AsyncClient, professor_user_headers, sessi
         "/groups/groups",
         headers=professor_user_headers,
     )
-    assert response.status_code == 200
-
-async def test_get_group(client: AsyncClient, professor_user_headers: dict):
-    class_id = "bd65600d-8669-4903-8a14-af88203add38" 
-    response = await client.get(
-        f"/groups/class?class_id={class_id}",
-        headers=professor_user_headers,
-    )
-    assert response.status_code == 200    
+    assert response.status_code == 200 
 
 async def test_create_group(client: AsyncClient, professor_user_headers, session: AsyncSession):
     class_data = {"name": "New Test Group"} 
@@ -36,3 +28,24 @@ async def test_create_group(client: AsyncClient, professor_user_headers, session
     group_in_db = result.scalar_one_or_none()
     assert group_in_db is not None
     assert group_in_db.name == class_data['name']
+
+async def test_create_and_get_group(client: AsyncClient, professor_user_headers, session: AsyncSession):
+    # Kreiranje grupe
+    class_data = {"name": "New Test Group"} 
+    response = await client.post(
+        "/groups/groups",
+        json=class_data,
+        headers=professor_user_headers,
+    )
+    assert response.status_code == 200
+    created_class = response.json()
+    class_id = created_class['id']
+
+    # Dobavljanje grupe
+    response = await client.get(
+        f"/groups/class?class_id={class_id}",
+        headers=professor_user_headers,
+    )
+    assert response.status_code == 200
+    fetched_class = response.json()
+    assert fetched_class['id'] == class_id
