21e8223c35351d854298c664ef6a50ce90002d49
VedadDe <96734106+VedadDe@users.noreply.github.com>
2024-01-09 14:16:12 +0100

    adde missing components

--- frontend/src/views/login-view.js
+++ frontend/src/views/login-view.js
@@ -56,18 +56,67 @@ export default function LoginView() {
 
 	return (
 		<main className={styles.main}>
-			{/* Other component s */}
+			<div className={styles.description}>
+				<div>
+					<a
+						href="https://pmf.unsa.ba"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						By{' PMF 2023/2024'}
+					</a>
+				</div>
+			</div>
 			<Grid
 				container
 				sx={{ alignContent: 'center', marginTop: 1 }}
 				direction="column"
 			>
-				{/* Other Grid items */}
+				<Grid item>
+					<div
+						style={{
+							marginLeft: 100,
+							backgroundImage: "url('/logo.png')",
+							backgroundRepeat: 'no-repeat',
+							height: 130,
+							width: 130,
+							backgroundSize: 'cover',
+							backgroundPosition: 'center',
+						}}
+					></div>
+				</Grid>
+
+				<Grid item xs={12}></Grid>
+
+				{/* <Grid item >
+          <Typography
+            variant="h4"
+            sx={{ marginTop: 4, textAlign: "center", color: "gray" }}
+          >
+            eŠkola matematike
+          </Typography>
+        </Grid> */}
+
 				<Grid item sx={{ marginTop: 1 }} className={styles.center}>
 					<Card sx={{ maxWidth: 345, backgroundColor: 'white', padding: 2 }}>
 						<CardContent>
 							<Grid container spacing={1}>
-								{/* Other Grid items */}
+								<Grid item xs={12}>
+									<Typography
+										variant="h6"
+										sx={{ textAlign: 'center', color: 'gray' }}
+									>
+										eŠkola matematike
+									</Typography>
+								</Grid>
+
+								<Grid item xs={12} mb={2}>
+									{error && (
+										<Alert severity="error">
+											{error?.response?.data || error?.message || error?.detail}
+										</Alert>
+									)}
+								</Grid>
 								<Grid item xs={12}>
 									<TextField
 										id="email"
