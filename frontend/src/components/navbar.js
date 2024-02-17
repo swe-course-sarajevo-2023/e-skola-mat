2e99fb0d8607beebce7c741d87a49ffe0b1c5076
imanBekkaye <imanbekkaye@hotmail.com>
2024-02-17 13:33:29 +0100

    menu for dif roles added

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -10,6 +10,12 @@ import Button from '@mui/material/Button';
 import Link from 'next/link';
 import { GetLoggedUser } from '@/api';
 import { useQuery } from 'react-query';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function ButtonAppBar() {
 	const { data } = useQuery(['getLoggedUser'], () => GetLoggedUser());
@@ -20,6 +26,22 @@ export default function ButtonAppBar() {
 		localStorage.removeItem('token');
 		router.push('/login');
 	};
+
+	const [anchorElIcon, setAnchorElIcon] = React.useState(null);
+    const [anchorElMenu, setAnchorElMenu] = React.useState(null);
+
+	const handleIconClick = (event) => {
+        setAnchorElIcon(event.currentTarget);
+    };
+
+    const handleMenuClick = (event) => {
+        setAnchorElMenu(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorElIcon(null);
+        setAnchorElMenu(null);
+    };
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
@@ -50,23 +72,110 @@ export default function ButtonAppBar() {
 					</Typography>
 
 					<Link
-						href={
-							data?.user_role === 'PROFESSOR'
-								? '/profiles/profesor'
-								: '/profiles/student'
-						}
-						passHref
-					>
-						<Button color="inherit">Početna</Button>
-					</Link>
+                        href={
+                            data?.user_role === 'PROFESSOR'
+                                ? '/profiles/profesor'
+                                : '/profiles/student'
+                        }
+                        passHref
+                    >
+                       <IconButton
+                        aria-label="more"
+                        aria-controls="long-menu"
+                        aria-haspopup="true"
+                        color="inherit"
+                    >
+                        <HomeIcon />
+                    </IconButton>
+                    </Link>
 
-					<Link href="/profiles/resetpassword" passHref>
-						<Button color="inherit">Promijeni lozinku</Button>
-					</Link>
+					{data?.user_role === 'PROFESSOR' ? (
+                        <>
+							<IconButton
+								aria-label="more"
+								aria-controls="long-menu"
+								aria-haspopup="true"
+								onClick={handleMenuClick}
+								color="inherit"
+							>
+								<MenuIcon />
+							</IconButton>
+								
+                            <Menu
+                                id="simple-menu"
+                                anchorEl={anchorElMenu}
+                                open={Boolean(anchorElMenu)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <Link href="/profiles/profesor/professor-homework" passHref>
+                                        Dodavanje/prikaz zadaća
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link href="/profiles/profesor" passHref>
+                                        Dodavanje/prikaz grupa
+                                    </Link>
+                                </MenuItem>
+                            </Menu>
+                        </>
+                    ) : (
+						<>
+						<IconButton
+							aria-label="more"
+							aria-controls="long-menu"
+							aria-haspopup="true"
+							onClick={handleMenuClick}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+							
+						<Menu
+							id="simple-menu"
+							anchorEl={anchorElMenu}
+							open={Boolean(anchorElMenu)}
+							onClose={handleClose}
+						>
+							<MenuItem onClick={handleClose}>
+								<Link href="/profiles/student" passHref>
+									Sve zadaće
+								</Link>
+							</MenuItem>
+							<MenuItem onClick={handleClose}>
+								<Link href="/profiles/student" passHref>
+									Poslane zadaće
+								</Link>
+							</MenuItem>
+							
+						</Menu>
+					</>
+                    )}
 
-					<Button color="inherit" onClick={handleLogout}>
-						LOGOUT
-					</Button>
+					<IconButton
+                        aria-label="more"
+                        aria-controls="long-menu"
+                        aria-haspopup="true"
+                        onClick={handleIconClick}
+                        color="inherit"
+                    >
+                        <AccountCircleIcon />
+                    </IconButton>
+
+                    <Menu
+                        id="long-menu"
+                        anchorEl={anchorElIcon}
+                        keepMounted
+                        open={Boolean(anchorElIcon)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>
+                            <Link href="/profiles/resetpassword" passHref>
+                                Promijeni lozinku
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={handleLogout}>LOGOUT</MenuItem>
+                    </Menu>
 				</Toolbar>
 			</AppBar>
 		</Box>
